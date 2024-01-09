4cb8d5c135248d93b18ee87d6373ec56c049b586
Yashhh03 <yashsurya8888@gmail.com>
2024-01-09 21:10:15 +0530

    Made some modifications

--- Javascript 1/index.js
+++ Javascript 1/index.js
@@ -1,1 +1,16 @@
 console.log('Namaste everyone');
+
+let marks=48;
+
+if(marks>90){
+    console.log('A');
+}
+else if(marks>80){
+    console.log('B');
+}
+else if(marks>70){
+    console.log('C');
+}
+else{
+    console.log('D')
+}
