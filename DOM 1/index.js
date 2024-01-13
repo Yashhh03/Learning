4525f152fc178b4ca5c1c1d5b82fb1da55bc1080
Yashhh03 <yashsurya8888@gmail.com>
2024-01-13 21:15:27 +0530

    done for the day

--- DOM 1/index.js
+++ DOM 1/index.js
@@ -26,3 +26,22 @@ thirdLink.addEventListener('click', function(event){
     event.preventDefault();
     console.log('Kuch Huva??');
 });
+
+
+
+let myDiv = document.createElement('div');
+
+function paraStatus(event){
+    console.log('Para '+ event.target.textContent);
+}
+
+myDiv.addEventListener('click',paraStatus);
+
+for(let i=1;i<100;i++){
+    let newElement = document.createElement('p');
+    newElement.textContent='This is para '+ i;
+
+    myDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myDiv);
