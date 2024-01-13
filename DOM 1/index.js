725e3f392b488ef3e81d5d25098a921390b641f3
Yashhh03 <yashsurya8888@gmail.com>
2024-01-13 20:02:16 +0530

    more done

--- DOM 1/index.js
+++ DOM 1/index.js
@@ -12,3 +12,17 @@ function print() {
 
 document.addEventListener('click', print);
 document.removeEventListener('click',print);
+
+const content = document.querySelector('#wrapper');
+
+content.addEventListener('click', function(event){
+    console.log(event);
+});
+
+let links = document.querySelectorAll('a');
+let thirdLink = links[2];
+
+thirdLink.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('Kuch Huva??');
+});
