007be728cb07da3cf49205e8c82ff50b9cd9e827
Yashhh03 <yashsurya8888@gmail.com>
2024-01-24 00:14:32 +0530

    just doing it

--- Express JS Learning/app.js
+++ Express JS Learning/app.js
@@ -1,17 +1,12 @@
-const http = require('http');
+const express = require('express');
 
-function handleRequest(request, response) {
+const app = express();
 
-    if(request.url==='/currenttime') {
-        response.statusCode = 200;
-        response.end('<h1>' + new Date().toISOString() +'</h1>');   
-    }
-    else if(request.url==='/') {
-        response.statusCode = 200;
-        response.end('<h1>Hello Everyone</h1>');   
-    }
-}
+app.get('/currenttime', function(req, res) {
+    res.send('<h1>' + new Date().toISOString() +'</h1>');
+});
+app.get('/',function(req,res) {
+    res.send('<h1>Hello 2 You</h1>');
+});
 
-const server = http.createServer(handleRequest);
-
-server.listen(3000);
+app.listen(3000);
