a6b589748509449b1e3fe11959691b686e76f1af
Yashhh03 <yashsurya8888@gmail.com>
2024-01-24 10:41:08 +0530

    got to know how to store data locally and nodemon

--- Express JS Learning/app.js
+++ Express JS Learning/app.js
@@ -1,12 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
 
 app.get('/currenttime', function(req, res) {
     res.send('<h1>' + new Date().toISOString() +'</h1>');
 });
 app.get('/',function(req,res) {
-    res.send('<h1>Hello 2 You</h1>');
+    res.send('<form action="/store-user" method="POST"><label>Your Name: </label><input type="text" name="username"><button>Submit</button></form>');
+});
+
+app.post('/store-user',function(req, res){
+    const userName = req.body.username;
+    const filePath = path.join(__dirname, 'data','users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUser = JSON.parse(fileData);
+    existingUser.push(userName);
+
+    fs.writeFileSync(filePath, JSON.stringify(existingUser));
+    res.send('<h1>Username Saved!</h1>')
+});
+
+app.get('/users', function(req,res){
+    const filePath = path.join(__dirname, 'data','users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUser = JSON.parse(fileData);
+
+    let responseData = '<ul>';
+    for(const user of existingUser) {
+        responseData += '<li>'+ user +'</li>'
+    }
+
+    responseData += '</ul>';
+
+    res.send(responseData);
 });
 
 app.listen(3000);
