2d6c1b7eaee0376811ded1e5a4cc394fe9cb8de0
Yashhh03 <yashsurya8888@gmail.com>
2024-01-31 21:18:59 +0530

    adding dependency for the login page

--- Auth Login Page/routes/demo.js
+++ Auth Login Page/routes/demo.js
@@ -23,6 +23,25 @@ router.post('/signup', async function (req, res) {
   const enteredConfirmEmail = userData['confirm-email'];
   const enteredPassword = userData.password;
 
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    console.log('Incorrect Data Entered');
+    return res.redirect('/signup');
+  }
+
+  const existingUser = await db.getDb().collection('users').findOne({email : enteredEmail});
+
+  if(existingUser) {
+    console.log('Existing User');
+    return res.redirect('/signup');
+  }
+
   const hashedPassword = await bcrypt.hash(enteredPassword,12);
 
   const user = {
