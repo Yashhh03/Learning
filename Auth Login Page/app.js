51a8584ca9db5d4d912c672a23f0cda146551a8b
Yashhh03 <yashsurya8888@gmail.com>
2024-02-01 18:16:11 +0530

    adding session-cookies

--- Auth Login Page/app.js
+++ Auth Login Page/app.js
@@ -1,17 +1,34 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
+const mongodbStore = require('connect-mongodb-session');
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
+const MongoDBStore = mongodbStore(session);
+
 const app = express();
+
+const SessionStore = new MongoDBStore({
+  uri: 'mongodb://localhost:27017',
+  databaseName: 'auth-demo',
+  collection: 'session'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: 'super-secret',
+  resave: false,
+  saveUninitialized: false,
+  store: SessionStore
+}));
 
 app.use(demoRoutes);
 
