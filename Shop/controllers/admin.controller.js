be4b663bfe4d4f58e11fda7d13d995a9e02ca05b
Yashhh03 <yashsurya8888@gmail.com>
2024-02-15 16:53:57 +0530

    adding multer

--- Shop/controllers/admin.controller.js
+++ Shop/controllers/admin.controller.js
@@ -7,8 +7,11 @@ function getNewProduct(req,res) {
     res.render('admin/products/new-products');
 };
 
-function createNewProduct() {
+function createNewProduct(req, res) {
+    console.log(req.body);
+    console.log(req.file);
 
+    res.redirect('/admin/products');
 };
 
 module.exports = {
