0b6c856ef480eeb75d0c1e0b1fd98203d5ba24b1
Yashhh03 <yashsurya8888@gmail.com>
2024-02-15 23:09:11 +0530

    update the existing products

--- Shop/models/product.model.js
+++ Shop/models/product.model.js
@@ -9,8 +9,7 @@ class Product {
     this.price = +productData.price;
     this.description = productData.description;
     this.image = productData.image; // the name of the image file
-    this.imagePath = `product-data/images/${productData.image}`;
-    this.imageUrl = `/products/assets/images/${productData.image}`;
+    this.updateImageData();
     if (productData._id) {
       this.id = productData._id.toString();
     }
@@ -32,7 +31,7 @@ class Product {
       throw error;
     }
 
-    return product;
+    return new Product(product);
   }
 
   static async findAll() {
@@ -41,6 +40,11 @@ class Product {
     return products.map(function(productDocument) {
       return new Product(productDocument);
     });
+  }
+
+  updateImageData() {
+    this.imagePath = `product-data/images/${this.image}`;
+    this.imageUrl = `/products/assets/images/${this.image}`;
   }
 
   async save() {
@@ -52,7 +56,24 @@ class Product {
       image: this.image
     };
 
-    await db.getDb().collection('products').insertOne(productData);
+    if(this.id) {
+      const productId = new mongodb.ObjectId(this.id);
+
+      if(!this.image) {
+        delete productData.image;
+      }
+
+      await db.getDb().collection('products').updateOne({_id: productId},{
+        $set:productData
+      });
+    }else {
+      await db.getDb().collection('products').insertOne(productData);
+    }
+  }
+
+  replaceImage(newImage) {
+    this.image = newImage;
+    this.updateImageData();
   }
 }
 
