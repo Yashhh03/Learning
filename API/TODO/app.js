2ae0e668697016912cd802501b94979e122419bb
Yashhh03 <yashsurya8888@gmail.com>
2024-02-17 12:49:48 +0530

    adding todo backend and frontend

--- API/TODO/app.js
+++ API/TODO/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const db = require('./data/database');
 const todosRoutes = require('./routes/todos.routes');
+const enableCors = require('./middlewares/cors');
 
 const app = express();
 
+app.use(enableCors);
 app.use(express.json());
 
-app.use('/todos',todosRoutes);
+app.use('/todos', todosRoutes);
 
 app.use(function (error, req, res, next) {
   res.status(500).json({
